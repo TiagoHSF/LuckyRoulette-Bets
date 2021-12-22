9f3f77a13e698ba2690d56369e732f048bf04d6b
Tiago Henrique da Silva <tiagohs.hotmart@gmail.com>
2021-12-22 18:57:36 -0300

    Design in progress

--- src/app/bet/bet.component.ts
+++ src/app/bet/bet.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-
+import Swal from 'sweetalert2'
+import 'sweetalert2/src/sweetalert2.scss'
 
 @Component({
   selector: 'app-bet',
@@ -7,20 +8,38 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./bet.component.scss']
 })
 export class BetComponent implements OnInit {
+  amount: number;
+  green: number;
+  red: number;
+
+  constructor() { 
+    this.amount = 4000;
+    this.green = -50;
+    this.red = 100;
+  }
 
   
-
-  constructor() { }
-
   ngOnInit(): void {
   }
 
-betGreen(){
-  console.log("bet green")
+betGreen() {
+  Swal.fire({
+    title: 'YOU BET IN GREEN!',
+    icon: 'success',
+    confirmButtonText: 'GG'
+  })
 }
 
-betRed(){
-  console.log("bet red")
+betRed() {
+  Swal.fire({
+    title: 'YOU BET IN RED!',
+    icon: 'success',
+    confirmButtonText: 'GG'
+  })
+}
+
+converter() {
+
 }
 
 }
